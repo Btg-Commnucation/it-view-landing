263019ae4b39f384f03d3bb9d94708473bbf73f1
Tuvdunyel <mightygodx@gmail.com>
2023-06-14 09:16:40 +0200

    mentions img

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php get_header('page'); ?>
 
 <main id="page">
-    <img src="<?= get_template_directory_uri(); ?>" alt="Barre de couleurs">
+    <img src="<?= get_template_directory_uri(); ?>/public/barre_de_couleurs_web.png" alt="Barre de couleurs">
     <?php get_template_part('./parts/content'); ?>
 </main>
 
